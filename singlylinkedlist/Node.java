ab757b1bc442cae2947dbaa57d30dbef8a30e946
Lisa Swart <lisswart@amazon.com>
2022-12-22 20:09:15 -0500

    first commit

---
+++ singlylinkedlist/Node.java
@@ -0,0 +1,19 @@
+package singlylinkedlist;
+
+public class Node {
+    public String data;
+    private Node next;
+
+    public Node(String data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public void setNextNode(Node node) {
+        this.next = node;
+    }
+
+    public Node getNextNode() {
+        return this.next;
+    }
+}
