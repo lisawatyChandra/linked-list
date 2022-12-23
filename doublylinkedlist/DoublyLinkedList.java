4c2296d351dd40da61e636bd577bfb3a243ab1a1
Lisa Swart <lisswart@amazon.com>
2022-12-22 21:31:02 -0500

    add comments to removeHead() and removeTail()

--- doublylinkedlist/DoublyLinkedList.java
+++ doublylinkedlist/DoublyLinkedList.java
@@ -29,7 +29,7 @@ public class DoublyLinkedList {
             newHead.setNextNode(currentHead);
         }
 
-        // if the list is empty, update the head property to be the new head
+        // update the head property to be the new head
         this.head = newHead;
 
         // if the list is empty, update the tail property to be the new head
@@ -40,33 +40,53 @@ public class DoublyLinkedList {
     }
 
     public void addToTail(String data) {
+        // create the new node that will be added to the tail of the list
         Node newTail = new Node(data);
+        // assign a pointer to the current tail
         Node currentTail = this.tail;
 
+        // if the list is not empty
         if (currentTail != null) {
+            // reset the pointers at the tail of the list:
+            // set the current tail's next node to be the new tail
             currentTail.setNextNode(newTail);
+            // set the new tail's previous node to the current tail
             newTail.setPreviousNode(currentTail);
         }
+        // update the tail property to be the new tail
         this.tail = newTail;
 
+        // if the list is empty, update the head property to be the new tail
+        // since that node will be both the head and the tail
         if (this.head == null) {
             this.head = newTail;
         }
     }
 
     public String removeHead() {
+        // since we will return the data contained in the removed head
+        // assign a pointer to the removed head, that is, current head
         Node removedHead = this.head;
 
+        // if current head is null, then the list is empty, so there is
+        // nothing to return, return null, and the method ends
         if (removedHead == null) {
             return null;
         }
+        // otherwise, update the list's head to be the current head's next node
         this.head = removedHead.getNextNode();
 
+        // if the list had more than one element when we started,
         if (this.head != null) {
+            // reassign this node's previous pointer to null to effectively
+            // remove the former head node from the head of the list
             this.head.setPreviousNode(null);
         }
 
+        // if there is only one element in the list, that is,
+        // if the removed head was also the tail of the list
         if (removedHead == this.tail) {
+            // call .removeTail() to make the necessary changes to the tail of the list
             this.removeTail();
         }
 
@@ -74,18 +94,30 @@ public class DoublyLinkedList {
     }
 
     public String removeTail() {
+        // since we will return the data contained in the removed tail
+        // assign a pointer to the removed tail, that is, current tail
         Node removedTail = this.tail;
 
+        // if current tail is null, then the list is empty, so there is
+        // nothing to return, return null, and the method ends
         if (removedTail == null) {
             return null;
         }
 
+        // otherwise, update the list's tail to be the current tail's previous node
         this.tail = removedTail.getPreviousNode();
+
+        // if the list had more than one element when we started
         if (this.tail != null) {
+            // reassign this node's next pointer to null to effectively
+            // remove the former tail node from the tail of the list
             this.tail.setNextNode(null);
         }
 
+        // if there is only one element in the list, that is,
+        // if the removed tail was also the head of the list
         if (removedTail == this.head) {
+            // call .removeHead() to make the necessary changes to the head of the list
             this.removeHead();
         }
 
